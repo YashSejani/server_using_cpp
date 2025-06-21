6bce0ea0bdb5e099d6248eec9d640ba96712a817
Shubham Chhatbar <150677135+Shubham-0-0-7@users.noreply.github.com>
2025-06-21 10:59:07 +0530

    added get content type method

--- src/server.cpp
+++ src/server.cpp
@@ -4,22 +4,38 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sstream>
-#include <fstream> 
+#include <fstream>
 
-std::string read_file(const std::string& filename) {
-    std::ifstream file(filename);
+using namespace std;
+
+string read_file(const string& filename) {
+    ifstream file(filename);
     if (!file) return "";
 
-    std::ostringstream ss;
+    ostringstream ss;
     ss << file.rdbuf();
     return ss.str();
+}
+bool ends_with(const string& value, const string& suffix) {
+    if (suffix.size() > value.size()) return false;
+    return equal(suffix.rbegin(), suffix.rend(), value.rbegin());
+}
+
+string get_content_type(const string& path) {
+    if (ends_with(path, ".html")) return "text/html";
+    if (ends_with(path, ".css")) return "text/css";
+    if (ends_with(path, ".js")) return "application/javascript";
+    if (ends_with(path, ".png")) return "image/png";
+    if (ends_with(path, ".jpg") || ends_with(path, ".jpeg")) return "image/jpeg";
+    if (ends_with(path, ".gif")) return "image/gif";
+    return "text/plain";
 }
 
 int main() {
     // 1. Create a socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
-        std::cerr << "Socket creation failed.\n";
+        cerr << "Socket creation failed.\n";
         return 1;
     }
 
@@ -30,47 +46,47 @@ int main() {
     address.sin_port = htons(8080);
 
     // 3. Bind socket to address
-    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed.\n";
+    if (::bind(server_fd, (sockaddr*)&address, sizeof(address)) < 0) {
+        cerr << "Bind failed.\n";
         return 1;
     }
 
     // 4. Start listening
     if (listen(server_fd, 5) < 0) {
-        std::cerr << "Listen failed.\n";
+        cerr << "Listen failed.\n";
         return 1;
     }
 
-    std::cout << "Server is running at http://localhost:8080\n";
+    cout << "Server is running at http://localhost:8080\n";
 
     // 5. Accept client
     sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
     int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
     if (client_fd < 0) {
-        std::cerr << "Accept failed.\n";
+        cerr << "Accept failed.\n";
         return 1;
     }
 
     // 6. Read request
     char buffer[2048] = {0};
     read(client_fd, buffer, sizeof(buffer));
-    std::cout << "Request:\n" << buffer << "\n";
+    cout << "Request:\n" << buffer << "\n";
 
     // 7. Parse request line
-    std::istringstream request_stream(buffer);
-    std::string method, path, http_version;
+    istringstream request_stream(buffer);
+    string method, path, http_version;
     request_stream >> method >> path >> http_version;
 
-    std::string response_body;
-    std::string status_line;
+    string response_body;
+    string status_line;
 
     if (method == "GET") {
         // Convert / to /index.html
         if (path == "/") path = "/index.html";
 
         // Remove leading /
-        std::string filename = "." + path;
+        string filename = "." + path;
 
         response_body = read_file(filename);
 
@@ -84,12 +100,12 @@ int main() {
         status_line = "HTTP/1.1 405 Method Not Allowed\r\n";
         response_body = "<h1>405 Method Not Allowed</h1>";
     }
-
+    string content_type = get_content_type(path);
     // 8. Build HTTP response
-    std::string response =
+    string response =
         status_line +
-        "Content-Type: text/html\r\n"
-        "Content-Length: " + std::to_string(response_body.length()) + "\r\n"
+        "Content-Type: " + content_type + "\r\n"
+        "Content-Length: " + to_string(response_body.length()) + "\r\n"
         "Connection: close\r\n"
         "\r\n" + response_body;
 
